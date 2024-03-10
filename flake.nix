83e4d9fe274410ad2159b8201bc80083d3156a63
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-03-11 03:08:18 +0800

    Removed nixvim (for now doesn't work with home-manager, will fix later.

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
   home-manager.url = "github:nix-community/home-manager";
   home-manager.inputs.nixpkgs.follows = "nixpkgs";
   # Nixvim
-  nixvim = {
-    url = "github:nix-community/nixvim";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  # nixvim = {
+  #   url = "github:nix-community/nixvim";
+  #   inputs.nixpkgs.follows = "nixpkgs";
+  # };
   # TODO: Add any other flake you might need
   # hardware.url = "github:nixos/nixos-hardware";
   # Shameless plug: looking for a way to nixify your themes and make
@@ -37,7 +37,7 @@
     homeConfigurations = {
     "virus-free" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance 
-      extraSpecialArgs = {inherit inputs outputs;};
+      extraSpecialArgs = {inherit inputs;};
       # > Our main home-manager configuration file <
       modules = [./cfg/home.nix];
       };
